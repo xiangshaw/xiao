9326e178fc7b380765bc10e77915acd52f501269
xiangshaw <coisini.xiang@qq.com>
2023-06-20 20:06:08 +0800

    复习了Kafka Stream流式处理，添加了部分代码注释

--- xiao-article/src/main/java/plus/axz/article/job/ComputeHotArticleJob.java
+++ xiao-article/src/main/java/plus/axz/article/job/ComputeHotArticleJob.java
@@ -1,30 +1,29 @@
-package plus.axz.article.job;
-
-import com.xxl.job.core.biz.model.ReturnT;
-import com.xxl.job.core.handler.annotation.XxlJob;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import plus.axz.article.service.HotArticlesService;
-
-/**
- * @author xiaoxiang
- * @date 2022年06月25日
- * @particulars 热点文章分值定时计算
- */
-@Component
-@Log4j2
-public class ComputeHotArticleJob {
-
-    @Autowired
-    private HotArticlesService hotArticlesService;
-
-    @XxlJob("xcomputeHotArticleJob")/*和xxljob 的JobHandler 填的一定要保持一致**/
-    public ReturnT<String> handle(String param) throws Exception {
-        log.info("热文章分值计算调度任务开始执行....");
-        hotArticlesService.computeHotArticle();
-        log.info("热文章分值计算调度任务开始执行....");
-
-        return ReturnT.SUCCESS;
-    }
-}
+//package plus.axz.article.job;
+//
+//import com.xxl.job.core.biz.model.ReturnT;
+//import com.xxl.job.core.handler.annotation.XxlJob;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import plus.axz.article.service.HotArticlesService;
+//
+///**
+// * @author xiaoxiang
+// * @date 2022年06月25日
+// * @particulars 热点文章分值定时计算
+// */
+//@Component
+//@Log4j2
+//public class ComputeHotArticleJob {
+//
+//    @Autowired
+//    private HotArticlesService hotArticlesService;
+//
+//    @XxlJob("xcomputeHotArticleJob")/*和xxljob 的JobHandler 填的一定要保持一致**/
+//    public ReturnT<String> handle(String param) throws Exception {
+//        log.info("热文章分值计算调度任务开始执行....");
+//        hotArticlesService.computeHotArticle();
+//        log.info("热文章分值计算调度任务开始执行....");
+//        return ReturnT.SUCCESS;
+//    }
+//}
--- xiao-article/src/main/java/plus/axz/article/service/impl/HotArticleServiceImpl.java
+++ xiao-article/src/main/java/plus/axz/article/service/impl/HotArticleServiceImpl.java
@@ -37,14 +37,12 @@ public class HotArticleServiceImpl implements HotArticlesService {
     // 定时 计算热点文章数据
     @Override
     public void computeHotArticle() {
-
         // 数据库要保证有前几天的数据
-
-        // 1.查询前5天的文章数据
+        // 1.查询前5天的文章数据（代码释义 拿到当前时间 减去 5 天，给个格式，返回的就是时间参数）
         String s = DateTime.now().minusDays(5).toString("yyyy-MM-dd 00:00:00");
-        // gt 大于 发布时间大于前五天的点
+        // gt 大于 发布时间大于前五天的点 （拿到前5天的文章数据）
         List<Article> articles = articleMapper.selectList(Wrappers.<Article>lambdaQuery().gt(Article::getPublishTime, s));
-        // 2.计算文章分值
+        // 2.计算文章分值（根据阅读、点赞、评论以及收藏的量）
         List<HotArticleVo> hotArticleVoList = computeHotArticle(articles);
         // 3.为每一个频道标签热点较高的30条文章
         cacheTagToRedis(hotArticleVoList);
@@ -63,12 +61,17 @@ public class HotArticleServiceImpl implements HotArticlesService {
         if (articles != null && !articles.isEmpty()) {
             for (Article article : articles) {
                 hotArticleVo = new HotArticleVo();
+                // article对象 拷贝到 hotArticleVo
                 BeanUtils.copyProperties(article, hotArticleVo);
+                // 某一篇文章的分值
                 Integer score = computeScore(article);
+                // score 封装到 hotArticleVo中
                 hotArticleVo.setScore(score);
+                // 将一个个对象装入集合中
                 resultList.add(hotArticleVo);
             }
         }
+        // 返回对象
         return resultList;
     }
 
@@ -82,15 +85,19 @@ public class HotArticleServiceImpl implements HotArticlesService {
         // 默认分值0
         Integer score = 0;
         if (article.getLikes() != null) {
+            // 喜欢量 的权重
             score += article.getLikes() * ArticleConstans.HOT_ARTICLE_LIKE_WEIGHT;
         }
         if (article.getViews() != null) {
+            // 浏览量 的权重
             score += article.getViews();
         }
         if (article.getComment() != null) {
+            // 评论量 的权重
             score += article.getComment() * ArticleConstans.HOT_ARTICLE_COMMENT_WEIGHT;
         }
         if (article.getCollection() != null) {
+            // 收藏量 的权重
             score += article.getCollection() * ArticleConstans.HOT_ARTICLE_COLLECTION_WEIGHT;
         }
         return score;
@@ -110,18 +117,18 @@ public class HotArticleServiceImpl implements HotArticlesService {
     private void cacheTagToRedis(List<HotArticleVo> hotArticleVoList) {
         // 1.查询所有频道标签
         ResponseResult responseResult = adminFeign.selectAllTag();
-        // 拿到所有频道标签信息
+        // 拿到所有频道标签信息 （先将JSON数据转为对象）
         List<Tag> tags = JSON.parseArray(JSON.toJSONString(responseResult.getData()), Tag.class);
-        // 2.检索出频道对应的文章列表，给每个频道存入30条缓存数据
+        // 2.检索出频道对应的文章列表，给每个频道 分值较高的 存30条数据 到Redis
         if (hotArticleVoList != null && !hotArticleVoList.isEmpty()) {
             for (Tag tag : tags) {
-                // 筛选后的数据
+                // 筛选后的数据 （筛选 hotArticleVoList文章中 哪些属于 当前频道标签数据，，查询出当前频道所有数据）
                 List<HotArticleVo> hotArticleVos = hotArticleVoList.stream().filter(x -> x.getTagId().equals(tag.getId())).collect(Collectors.toList());
-                // 3.给每个频道进行缓存
+                // 3.给每个频道进行缓存 （频道标签名称 HOT_ARTICLE_FIRST_PAGE + 频道标签的ID值
                 sortAndCache(hotArticleVos, ArticleConstans.HOT_ARTICLE_FIRST_PAGE + tag.getId());
             }
         }
-        // 4.给推荐频道缓存30条数据  所有文章排序之后的前30条
+        // 4.给推荐频道缓存30条数据_all_  所有文章排序之后的前30条存入Redis （key 是频道标签
         sortAndCache(hotArticleVoList, ArticleConstans.HOT_ARTICLE_FIRST_PAGE + ArticleConstans.DEFAULT_TAG);
     }
 
@@ -133,17 +140,19 @@ public class HotArticleServiceImpl implements HotArticlesService {
      * @date 2022/6/25
      */
     private void sortAndCache(List<HotArticleVo> hotArticleVos, String s) {
+        // sort排序
         hotArticleVos.sort(new Comparator<HotArticleVo>() {
             @Override
             public int compare(HotArticleVo o1, HotArticleVo o2) {
                 return o2.getScore().compareTo(o1.getScore());
             }
         });
+        // 大于30条数据，只要30条
         if (hotArticleVos.size() > 30) {
+            // 再存入集合中
             hotArticleVos = hotArticleVos.subList(0, 30);
         }
-        /*hotArticleVos.size==0存入redis缓存*/
+        // hotArticleVos.size==0 直接存入redis缓存 （key 就是频道标签ID
         redisTemplate.opsForValue().set(s, JSON.toJSONString(hotArticleVos));
     }
-
 }
